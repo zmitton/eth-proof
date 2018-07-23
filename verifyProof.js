4d099cb50ffc07245fb14e55b1dfd98bfa6cb926
Shirikatsu <cc@clearmatics.com>
2018-07-23 14:00:31 +0100

    Added logging around trie deconstruction

--- verifyProof.js
+++ verifyProof.js
@@ -142,6 +142,7 @@ VerifyProof.trieValue = (path, value, parentNodes, root) => {
 
     for (var i = 0 ; i < len ; i++) {
       currentNode = parentNodes[i];
+      console.log("RAW NODE: " + currentNode);
       console.log("RLP ENCODED NODE = 0x" + rlp.encode(currentNode).toString('hex'));
       console.log("HASHED NODE = 0x" + sha3(rlp.encode(currentNode)).toString('hex'));
       if(!nodeKey.equals( Buffer.from(sha3(rlp.encode(currentNode)),'hex'))){
