e1f841d15370275e061c99f9c8b6e8dfd3c0f972
Zac Mitton <zacmitton22@gmail.com>
2019-02-18 12:30:28 -0400

    remove consolelog

--- test/account.js
+++ test/account.js
@@ -65,51 +65,49 @@ describe('Account GetAndVerify Against BlockHash', () => {
     let blockHash = '0xb7964f87a97582605af695710ad252afa018a97384ba9438cf24e42fa9f0efc9'
     let accountAddress = '0x9cc9bf39a84998089050a90087e597c26758685d'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHash)
-    console.log(account.serialize())
-    console.log(account.toHex())
   });
   it('should be able to request a proof for 0xb53f7522 and verify it', async () => {
     let blockHash = '0x7315156cc8347cf9bfac8a4cd1db6f5d4727b06b3fe14feba62381335b2d14d5'
     let accountAddress = '0x9cc9bf39a84998089050a90087e597c26758685d'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHash)
-    // console.log(prf)
+    // console.log(account)
   });
   it('should be able to prove an account does not exist', async () => {
     let blockHashBlockZero = '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
     let emptyAccountAddress = '0x0000000000000000000000000000000000000000'
     let account = await getAndVerify.accountAgainstBlockHash(emptyAccountAddress, blockHashBlockZero)
-    // console.log(prf)
+    // console.log(account)
   });
 //standard accounts validated by random legitimate blockhash
   it('should be able to request a proof for 0xb53f7522 and verify it', async () => {
     let blockHash = '0x7315156cc8347cf9bfac8a4cd1db6f5d4727b06b3fe14feba62381335b2d14d5'
     let accountAddress = '0x2a956e2fdcf3e338d0e925c68bcb73e7c8bb86c4'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHash)
-    // console.log(prf)
+    // console.log(account)
   });
   it('should be able to request a proof for 0xb53f7522 and verify it', async () => {
     let blockHash = '0x7315156cc8347cf9bfac8a4cd1db6f5d4727b06b3fe14feba62381335b2d14d5'
     let accountAddress = '0x1F4E7Db8514Ec4E99467a8d2ee3a63094a904e7A'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHash)
-    // console.log(prf)
+    // console.log(account)
   });
   it('should be able to request a proof for 0xb53f7522 and verify it', async () => {
     let blockHash = '0x7315156cc8347cf9bfac8a4cd1db6f5d4727b06b3fe14feba62381335b2d14d5'
     let accountAddress = '0x1F4E7Db8514Ec4E99467a8d2ee3a63094a904e7A'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHash)
-    // console.log(prf)
+    // console.log(account)
   });
   it('should be able to request a proof for 0xb53f7522 and verify it', async () => {
     let blockHash = '0x7315156cc8347cf9bfac8a4cd1db6f5d4727b06b3fe14feba62381335b2d14d5'
     let accountAddress = '0x0087194a367D4D508D9a97846264f69d81e419ca'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHash)
-    // console.log(prf)
+    // console.log(account)
   });
   it('should be able to request a proof for 0xb53f7522 and verify it', async () => {
     let blockHash = '0x7315156cc8347cf9bfac8a4cd1db6f5d4727b06b3fe14feba62381335b2d14d5'
     let accountAddress = '0x0d8775f648430679a709e98d2b0cb6250d2887ef'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHash)
-    // console.log(prf)
+    // console.log(account)
   });
 
 //   it('can verify proof', async () => {
@@ -124,7 +122,7 @@ describe('Account GetAndVerify Against BlockHash', () => {
     let blockHash = '0x7315156cc8347cf9bfac8a4cd1db6f5d4727b06b3fe14feba62381335b2d14d5'
     let accountAddress = '0xc1cdc601f89c0428b31302d187e0dc08ad7d1c57'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHash)
-    // console.log(prf)
+    // console.log(account)
   });
 
   it('should be able to prove an account did not exist at a given time', async () => {
@@ -132,7 +130,7 @@ describe('Account GetAndVerify Against BlockHash', () => {
     let blockHashBlockZero = '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
     let accountAddress = '0x9cc9bf39a84998089050a90087e597c26758685d'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHashBlockZero)
-    // console.log(prf)
+    // console.log(account)
     encode(account).equals(Account.NULL.toBuffer()).should.be.true()
 
   });
@@ -141,7 +139,7 @@ describe('Account GetAndVerify Against BlockHash', () => {
     let blockHashBlockZero = '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
     let accountAddress = '0xdeadbeef00123456789012345678901234567890'
     let account = await getAndVerify.accountAgainstBlockHash(accountAddress, blockHashBlockZero)
-    // console.log(prf)
+    // console.log(account)
     encode(account).equals(Account.NULL.toBuffer()).should.be.true()
     // account[0].equals(toBuffer()).should.be.true()
     // account[1].equals(toBuffer()).should.be.true()
